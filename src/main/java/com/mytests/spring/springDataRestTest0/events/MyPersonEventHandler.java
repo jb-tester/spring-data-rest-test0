e61ee9f0be50ccd4768008b96288718d39ed6f8e
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-11-16 10:02:37 +0100

    more event handlers - just to check that they are not shown as unused

--- src/main/java/com/mytests/spring/springDataRestTest0/events/MyPersonEventHandler.java
+++ src/main/java/com/mytests/spring/springDataRestTest0/events/MyPersonEventHandler.java
@@ -2,9 +2,7 @@ package com.mytests.spring.springDataRestTest0.events;
 
 
 import com.mytests.spring.springDataRestTest0.model.Person;
-import org.springframework.data.rest.core.annotation.HandleAfterCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeDelete;
-import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.annotation.*;
 import org.springframework.stereotype.Component;
 // should not be shown as not used
 @Component
@@ -14,12 +12,54 @@ public class MyPersonEventHandler {
   // should navigate to org.springframework.data.rest.core.event.AfterCreateEvent publisher
   @HandleAfterCreate
   public void handlePersonCreation(Person p) {
+
     System.out.println("new Person has been created with id="+p.getId());
   }
+  @HandleAfterSave
+  public void handlePersonSave(Person p) {
 
+    System.out.println("new Person has been saved with id="+p.getId());
+  }
+  @HandleAfterDelete
+  public void handlePersonDeleting(Person p) {
+
+    System.out.println("Person has been deleted");
+  }
+  @HandleAfterLinkSave
+  public void handleLinkSaving(Person p) {
+
+    System.out.println("link has been saved");
+  }
+  @HandleAfterLinkDelete
+  public void handleLinkDeleting(Person p) {
+
+    System.out.println("link has been deleted");
+  }
+
+  @HandleBeforeCreate
+  public void handleBeforePersonCreation(Person p) {
+
+    System.out.println("new Person will be created with id="+p.getId());
+  }
+  @HandleBeforeSave
+  public void handleBeforePersonSave(Person p) {
+
+    System.out.println("new Person will be saved with id="+p.getId());
+  }
   // should navigate to org.springframework.data.rest.core.event.BeforeDeleteEvent publisher
   @HandleBeforeDelete
   public void handlePersonDeleteAttempt(Person p){
+
     System.out.println("i'm trying to delete the person with id="+ p.getId());
+  }
+  @HandleBeforeLinkSave
+  public void handleBeforeLinkSaving(Person p) {
+
+    System.out.println("link will be saved");
+  }
+  @HandleBeforeLinkDelete
+  public void handleBeforeLinkDeleting(Person p) {
+
+    System.out.println("link will be deleted");
   }
 }
